08a9b886d0889aa54e28857bb1c5bd05302e1a00
anupriyaa9 <dpsvn.anup14073@gmail.com>
2021-10-01 16:07:51 +0530

    new commit

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@ let weather = {
     apiKey: "686be82807926e5e8497d9a3ad044e94",
     fetchWeather: function (city) {
       fetch(
-        api_url1= "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+this.apiKey,
+        api1= "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+this.apiKey,
 
       )
         .then((response) => {
           if (!response.ok) {
-            alert("No Weather found.");
-            throw new Error("No Weather found.");
+            alert("No weather found.");
+            throw new Error("No weather found.");
           }
           return response.json();
         })
@@ -23,6 +23,7 @@ let weather = {
       const { sunrise } = data.sys;
       const { sunset } = data.sys;
       const { temp_max, temp_min, pressure } = data.main;
+      const { lat, lon } = data.coord;
       document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
@@ -34,7 +35,30 @@ let weather = {
       document.querySelector(".temp_ma").innerText = temp_max + "°C";
       document.querySelector(".temp_mi").innerText = temp_min + "°C";
       document.querySelector(".press").innerText =  pressure + " hPa";
+      document.querySelector(".lat").innerText = "Lat: "+lat; document.querySelector(".lon").innerText = "Lon: "+lon;
     },
+
+
+    fetchWeekly: function (lat, lon) {
+      fetch(
+        api2= "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude="+minutely+"&appid="+this.apiKey,)
+        .then((response) => {
+          if (!response.ok) {
+            alert("No forecast found.");
+            throw new Error("No forecast found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeekly(data));
+    },
+
+    displayWeekly: function (data) {
+      const { lat, lon } = data.coord;
+      const { temp } = data.daily.temp.morn;
+      document.querySelector(".lat").innerText =  lat; document.querySelector(".lon").innerText =  lon;
+      document.querySelector(".mb-0").innerText = temp+"K";
+    },
+
     search: function () { 
       this.fetchWeather(document.querySelector(".search-bar").value);
     },
